1ddbde08042af14c93d414434d924c7d7122bc4c
Tharushi1212 <it20000608@my.sliit.lk>
2023-10-04 12:40:40 +0530

    #commit frontend dashboard header and welcome section done.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import '../assets/css/styles.css';
+import bellOff from '../assets/images/Bell-off.png';
+import profileImage from '../assets/images/Ellipse 6.png';
+import profileExpandArrow from '../assets/images/Chevron-down.png';
+import closeIcon from '../assets/images/Close.png';
+import welocmeSectionImage from '../assets/images/Group 1770.png';
+
+import Task from '../components/Task';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Fragment>
+      <div className="dashboard-wrapper">
+        <div className="acmy-sloution-section">
+          <div className="acmy-sloutions-header">
+            <text className="acmy-sloutions">Acmy Solutions</text>
+          </div>
+          <div className="acmy-solutions-body">body</div>
+        </div>
+        <div className="dashboard-section">
+          <div className="dashboard-header">
+            <text className="dashboard-text">Dashboard</text>
+            <div className="dashboard-header-profile">
+              <img src={bellOff} alt="belloff" className="bellOff"></img>
+              <img src={profileImage} alt="profileImage"></img>
+              <img src={profileExpandArrow} alt="profileExpandArrow"></img>
+            </div>
+          </div>
+          <div className="dashboard-body">
+            <div className="dashboard-wellcome-wrapper">
+              <div className="wellcome-text-wrapper">
+                <text className="welcome-text">Welcome back, John Doe</text>
+                <text className="welcome-sub-tex">
+                  The end of the year is coming. Are you planning your
+                  performance interviews? You can do this super efficiently with
+                  Acmy.
+                </text>
+                <text className="welcome-underline-text">
+                  Look here for more information
+                </text>
+              </div>
+              <img
+                src={welocmeSectionImage}
+                alt="welocmeSectionImage"
+                className="welocmeSectionImage"
+              ></img>
+              <img src={closeIcon} alt="closeIcon" className="closeIcon"></img>
+            </div>
+            <div className="task-wrapper">
+              <Task />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Dashboard;
